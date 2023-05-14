e3fae9f62ecf2180f5d212235eca87cad293be7b
loidinhm31 <loidinh.git@gmail.com>
2023-05-14 18:43:49 +0700

    Reference Movie: add search reference movies; and save a reference movie

---
+++ movies-front-end/src/pages/api/v1/admin/movies/references/[id]/index.ts
@@ -0,0 +1,23 @@
+import {withAnyRole} from "src/libs/auth";
+
+const handler = withAnyRole(["admin", "moderator"], async (req, res, token) => {
+    const {id} = req.query;    
+
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", `Bearer ${token.accessToken}`)
+
+    const requestOptions = {
+        method: "GET",
+        headers: headers,
+    };
+
+    const response = await fetch(`${process.env.API_BASE_URL}/integration/tmdb/${id}`, requestOptions);
+    if (response.ok) {
+        res.status(200).json(await response.json());
+    } else {
+        res.status(response.status).json(await response.json())
+    }
+});
+
+export default handler;
