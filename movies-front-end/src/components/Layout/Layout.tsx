8490dbfd8c07d50482543ae366ca3e25d5b5e86f
loidinhm31 <loidinh.git@gmail.com>
2023-05-18 08:38:11 +0700

    Layout: add user option into layout

--- movies-front-end/src/components/Layout/Layout.tsx
+++ movies-front-end/src/components/Layout/Layout.tsx
@@ -12,6 +12,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
 import ClassIcon from "@mui/icons-material/Class";
 import LinkIcon from "@mui/icons-material/Link";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
 
 export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: React.ReactElement) => React.ReactNode;
@@ -55,9 +56,15 @@ export const getDefaultLayout = (page: React.ReactElement) => (
                     icon: DashboardIcon,
                 },
                 {
-                    label: "Manage Catalogue",
+                    label: "User Management",
+                    pathname: "/admin/manage-user",
+                    desc: "User Management",
+                    icon: SupervisedUserCircleIcon,
+                },
+                {
+                    label: "Catalogue Management",
                     pathname: "/admin/manage-catalogue",
-                    desc: "Manage Catalogue",
+                    desc: "Catalogue Management",
                     icon: LibraryBooksIcon,
                 },
                 {
