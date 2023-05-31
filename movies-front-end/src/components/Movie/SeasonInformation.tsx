810c8552b2bcf3a3972cd11cfd50d9d49098aa0e
loidinhm31 <loidinh.git@gmail.com>
2023-06-01 00:37:07 +0700

    Season: Fix display with non-content

--- movies-front-end/src/components/Movie/SeasonInformation.tsx
+++ movies-front-end/src/components/Movie/SeasonInformation.tsx
@@ -1,13 +1,12 @@
-import {Accordion, AccordionDetails, AccordionSummary, Grid, IconButton, Paper, Typography} from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
+import {Accordion, AccordionDetails, AccordionSummary, Grid, Paper, Typography} from "@mui/material";
 import {format} from "date-fns";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {EpisodesTable} from "../Tables/EpisodesTable";
+import {EpisodesTable} from "src/components/Tables/EpisodesTable";
 import React from "react";
-import useSWRMutation from "swr/mutation";
-import {SeasonType} from "../../types/seasons";
-import {get} from "../../libs/api";
+import {SeasonType} from "src/types/seasons";
+import {get} from "src/libs/api";
 import useSWR from "swr";
+
 interface SeasonInformationProps {
     movieId: number;
 }
@@ -16,47 +15,49 @@ export default function SeasonInformation({movieId}: SeasonInformationProps) {
     const {data: seasons} = useSWR<SeasonType[]>(`/api/v1/seasons?movieId=${movieId}`, get)
 
     return (
-        <Grid item xs={12} md={10}>
-            <Paper elevation={5} sx={{p: 1}}>
-                {seasons && seasons.map((s, index) => (
-                    <Paper
-                        key={`${s.id}-${index}`}
-                        elevation={3}
-                        sx={{
-                            m: 2,
-                            p: 2,
-                            flexGrow: 1,
-                        }}
-                    >
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <Typography variant="subtitle1"><b>{s.name}</b></Typography>
+        <>
+            {seasons && seasons.map((s, index) => (
+                <Grid item xs={12} md={10}>
+                    <Paper elevation={5} sx={{p: 1}}>
+                        <Paper
+                            key={`${s.id}-${index}`}
+                            elevation={3}
+                            sx={{
+                                m: 2,
+                                p: 2,
+                                flexGrow: 1,
+                            }}
+                        >
+                            <Grid container spacing={2}>
+                                <Grid item xs={6}>
+                                    <Typography variant="subtitle1"><b>{s.name}</b></Typography>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Typography
+                                        variant="subtitle2">{format(new Date(s.air_date!), "MMMM do, yyyy")}</Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="body1">{s.description}</Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Accordion TransitionProps={{unmountOnExit: true}}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon/>}
+                                        >
+                                            <Typography variant="caption">Episodes</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <EpisodesTable
+                                                season={s}
+                                            />
+                                        </AccordionDetails>
+                                    </Accordion>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={4}>
-                                <Typography
-                                    variant="subtitle2">{format(new Date(s.air_date!), "MMMM do, yyyy")}</Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography variant="body1">{s.description}</Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Accordion TransitionProps={{unmountOnExit: true}}>
-                                    <AccordionSummary
-                                        expandIcon={<ExpandMoreIcon/>}
-                                    >
-                                        <Typography variant="caption">Episodes</Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <EpisodesTable
-                                            season={s}
-                                        />
-                                    </AccordionDetails>
-                                </Accordion>
-                            </Grid>
-                        </Grid>
+                        </Paper>
                     </Paper>
-                ))}
-            </Paper>
-        </Grid>
+                </Grid>
+            ))}
+        </>
     );
 }
